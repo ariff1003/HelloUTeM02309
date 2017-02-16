751e40ce88cd205cf9952a9423474d7df97abfbd
ARIFF BRAHIN <noor_ariff85@yahoo.com>
2017-02-15 22:18:28 -0600

    change color

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			</td>
 			<td>
 				<h1 id = "message"><?php echo "Hello UTeM!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				<p class='description'></p> Thanks for creating a <span class="red">PHP Starter Application</span>.
 			</td>
 		</tr>
 	</table>
